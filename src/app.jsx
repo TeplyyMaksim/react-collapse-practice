e3b090754ceca93d564d9b63778be1d5cc01661b
Maksim <teplyy.maksim@gmail.com>
2017-09-21 01:10:57 +0300

    Finish testing and make preparations to implement

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import Collapsible from 'react-collapsible';
 import '../styles/index.scss';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>It Works!</h1>
-        <p>This React project just works including <span className="redBg">module</span> local styles.</p>
-        <p>Enjoy!</p>
-      </div>
+			<div className="collapse-wrapper">
+				<Collapsible trigger={<div><span>Hello there!</span><i className="fa fa-angle-up"></i></div>} open={true}>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, ipsum, fuga, in, obcaecati magni ullam nobis voluptas fugiat tenetur voluptatum quas tempora maxime rerum neque deserunt suscipit provident cumque et mollitia ex aspernatur porro minus sapiente voluptatibus eos at perferendis repellat odit aliquid harum molestias ratione pariatur adipisci. Aliquid, iure.</p>
+				</Collapsible>
+			</div>
     )
   }
 }
